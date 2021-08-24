7a1d52ee8d7249a5365e58f10cd69222c107ed7b
Ganning Xu <ganningxu@gmail.com>
2021-08-24 09:26:57 -0400

    bubble sort test

--- .bit/tests/test_bubble_sort.js
+++ .bit/tests/test_bubble_sort.js
@@ -1,1 +1,31 @@
-console.log("running test_bubble_sort.js");
+const args = require('minimist')(process.argv.slice(2))
+const functions = require('./functions');
+const user = args['user'];
+const repo = args['repo'];
+
+async function main() {
+    try { bubble_sort = require('./../../bubble_sort.js') }
+    catch (e) {
+        console.log("Searching for 'bubble_sort.js'... file cannot be found");
+        await functions.throwError("Searching for 'bubble_sort.js'... file cannot be found", user, repo)
+
+        process.exit(1)
+    }
+
+
+    let testArray = [234, 43, 55, 63, 5, 6, 235, 547];
+    let correctRes = [5, 6, 43, 55, 63, 234, 235, 547];
+
+    let user_bubble = bubble_sort(testArray);
+
+    if (JSON.stringify(user_bubble) != JSON.stringify(correctRes)) {
+        console.log(`Got: "${user_bubble}", was expecting: "${correctRes}".`)
+        await functions.throwError(`Got: '${user_bubble}', was expecting: '${correctRes}'.`, user, repo)
+        console.log(`Got: "${user_bubble}", was expecting: "${correctRes}".`)
+        process.exit(1)
+    }
+
+    console.info("Yay! 🎉🎉🎉🎉🎉🎉🎉")
+}
+
+main();
--- bubble_sort.js
+++ bubble_sort.js
@@ -1,1 +1,23 @@
-// start coding your function!
+function bubble_sort(arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+
+        // Last i elements are already in place  
+        for (var j = 0; j < (arr.length - i - 1); j++) {
+
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (arr[j] > arr[j + 1]) {
+
+                // If the condition is true then swap them
+                var temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    // Print the sorted array
+    return arr;
+}
+
+module.exports = bubble_sort
