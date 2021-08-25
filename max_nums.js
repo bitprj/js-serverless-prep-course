da37c72cbf83b7771209ceded224d6f3284d0bd3
Ganning Xu <ganningxu@gmail.com>
2021-08-24 20:29:36 -0400

    add max num test

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,12 @@
-// start coding your function!
+function find_max(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+exports.find_max = find_max
