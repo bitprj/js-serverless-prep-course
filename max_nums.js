cd1ca62d66619f9d426a36be522b8f4f73e28c1e
Ganning Xu <ganningxu@gmail.com>
2021-08-25 21:19:51 -0400

    remove solutions

--- max_nums.js
+++ max_nums.js
@@ -1,12 +1,1 @@
-function find_max(arr) {
-    let max = arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-    }
-
-    return max;
-}
-
-exports.find_max = find_max
+// start coding your function here!
--- word_counter.js
+++ word_counter.js
@@ -1,5 +1,1 @@
-function word_counter(sentence) {
-    return sentence.split(" ").length;
-}
-
-exports.word_counter = word_counter;
+// start coding your function here!
